3a6fbecd33ba839480e178ecb83e4d52ce7d879b
enmanuel1363 <enmanuelzamora1363@gmail.com>
2025-10-01 21:31:16 -0600

    primer version de la pagina con las rutas de las pantallas

--- App.py
+++ App.py
@@ -4,11 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')  # Ruta principal
 def login():
-    return render_template('Ofertas/Ofertas.html')  # <- solo la subcarpeta
+    return render_template('Pantallas/Ofertas.html')  # <- solo la subcarpeta
+
+@app.route('/chatbox')  
+def chatbox():
+    return render_template('Pantallas/Chatbox.html')
+
+@app.route('/mapa')
+def mapa():
+    return render_template('Pantallas/Mapa.html')
 
 @app.route('/ofertas')  
 def ofertas():
-    return render_template('Ofertas/Ofertas.html')
+    return render_template('Pantallas/Ofertas.html')
+
+@app.route('/rci')
+def rci():
+    return render_template('Pantallas/RCI.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
